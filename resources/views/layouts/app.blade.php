73d0819f03dc692eba0a21a5caccc1883dbb91ef
Henry Lee <henryleeworld@gmail.com>
2023-12-01 00:18:32 +0800

    feat:更新最新資料

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,11 +10,9 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    @vite(['resources/sass/app.scss'])
     @livewireStyles
 </head>
 <body>
@@ -77,6 +75,8 @@
             @yield('content')
         </main>
     </div>
+    <!-- Scripts -->
+    @vite(['resources/js/app.js'])
     @livewireScripts
 </body>
 </html>
--- resources/views/livewire/country-state-city.blade.php
+++ resources/views/livewire/country-state-city.blade.php
@@ -3,7 +3,7 @@
         <label for="country" class="col-md-4 col-form-label text-md-right">{{ trans('frontend.houses.create.content.country') }}</label>
 
         <div class="col-md-6">
-            <select wire:model="selectedCountry" class="form-control">
+            <select wire:model.live="selectedCountry" class="form-control">
                 <option value="" selected>{{ trans('frontend.houses.create.content.choose_country') }}</option>
                 @foreach($countries as $country)
                     <option value="{{ $country->id }}">{{ $country->name }}</option>
@@ -16,7 +16,7 @@
             <label for="state" class="col-md-4 col-form-label text-md-right">{{ trans('frontend.houses.create.content.state') }}</label>
 
             <div class="col-md-6">
-                <select wire:model="selectedState" class="form-control">
+                <select wire:model.live="selectedState" class="form-control">
                     <option value="" selected>{{ trans('frontend.houses.create.content.choose_state') }}</option>
                     @foreach($states as $state)
                         <option value="{{ $state->id }}">{{ $state->name }}</option>
@@ -31,7 +31,7 @@
             <label for="city" class="col-md-4 col-form-label text-md-right">{{ trans('frontend.houses.create.content.city') }}</label>
 
             <div class="col-md-6">
-                <select wire:model="selectedCity" class="form-control" name="city_id">
+                <select wire:model.live="selectedCity" class="form-control" name="city_id">
                     <option value="" selected>{{ trans('frontend.houses.create.content.choose_city') }}</option>
                     @foreach($cities as $city)
                         <option value="{{ $city->id }}">{{ $city->name }}</option>
