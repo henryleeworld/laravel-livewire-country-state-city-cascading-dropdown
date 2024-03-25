f1b1bc287be2a23b100356a179395faf02d9aa66
Henry Lee <henryleeworld@gmail.com>
2024-03-26 00:06:07 +0800

    feat:升級至 laravel 11

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
     @vite(['resources/sass/app.scss'])
     @livewireStyles
 </head>
